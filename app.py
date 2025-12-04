0b983f4ee49ba991fe2f9e5de8a507497b1f15a4
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-12-04 18:21:27 +0530

    last update

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def handle_preflight():
 # ----------------------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render provides PORT
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
