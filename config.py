e8b9ee70b0cf32fa162950932a4492dd82c609e9
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-12-05 12:54:20 +0530

    12 54

--- config.py
+++ config.py
@@ -2,11 +2,9 @@ import os
 from urllib.parse import quote_plus
 
 class Config:
-    # Encode password to handle special characters like @
-    PASSWORD = quote_plus("Abcd@1234")
-
-    # Use encoded password in Mongo URI
-    MONGO_URI = f"mongodb+srv://paramjeet:{PASSWORD}@cluster0.patrtp0.mongodb.net/flask_auth_db?retryWrites=true&w=majority"
+    
+    
+    MONGO_URI = f"MONGO_URI= mongodb+srv://paramjeet:Abcd%401234@cluster0.patrtp0.mongodb.net/flask_auth_db?retryWrites=true&w=majority"
     print("MONGO_URI=", MONGO_URI)
     # Secret key for Flask sessions
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key_here")
