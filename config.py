ac9d0e3e8d48c0102d62df4c80a03d24ed4645db
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-12-05 12:55:09 +0530

    12 55

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from urllib.parse import quote_plus
 class Config:
     
     
-    MONGO_URI = f"MONGO_URI= mongodb+srv://paramjeet:Abcd%401234@cluster0.patrtp0.mongodb.net/flask_auth_db?retryWrites=true&w=majority"
+    MONGO_URI = f"mongodb+srv://paramjeet:Abcd%401234@cluster0.patrtp0.mongodb.net/flask_auth_db?retryWrites=true&w=majority"
     print("MONGO_URI=", MONGO_URI)
     # Secret key for Flask sessions
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key_here")
