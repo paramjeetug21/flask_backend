0403827c911448e5d58d0d3b1487c7f08a368bc0
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-12-05 12:18:13 +0530

    fix mongourl

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
 
     # Use encoded password in Mongo URI
     MONGO_URI = f"mongodb+srv://paramjeet:{PASSWORD}@cluster0.patrtp0.mongodb.net/flask_auth_db?retryWrites=true&w=majority"
-
+    print("MONGO_URI=", MONGO_URI)
     # Secret key for Flask sessions
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key_here")
