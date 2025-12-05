728c673e19c2ae3374881276a18c229e39a189d7
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-12-05 13:03:37 +0530

    1 03

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -15,5 +15,29 @@ def create_app():
      
     # Register blueprint
     app.register_blueprint(auth_bp, url_prefix="/auth")
+from flask import Flask
+from flask_cors import CORS
+from .router import auth_bp
+from .models import mongo
+from config import Config
+from flask_pymongo import PyMongo
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    CORS(app)
+
+    # Initialize Mongo with the app
+    mongo.init_app(app)  # use init_app, don't overwrite mongo
+    @app.route("/test-mongo")
+    def test_mongo():
+        try:
+            return {"databases": mongo.cx.list_database_names()}
+        except Exception as e:
+            return {"error": str(e)}  
+    # Register blueprint
+    app.register_blueprint(auth_bp, url_prefix="/auth")
 
     return app
+
+
